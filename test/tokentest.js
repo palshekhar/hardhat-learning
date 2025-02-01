b0c5d376b5fd4a26172df8b3fb280c61852783ed
palshekhar <palshekhar3021@gmail.com>
2025-02-02 01:04:39 +0530

    soo here i added new test for contract

--- test/tokentest.js
+++ test/tokentest.js
@@ -1,8 +1,32 @@
-const{expect}=require("chai");
+import { expect } from "chai";
+
+// const { ethers } = require("hardhat");
 // const{ethers}=require("ethers");
 
 
 describe("token contract",function(){
-    
+    let token;
+    let hardhattoken;
+    let owner;
+    let add1;
+    let add2;
+    let addrs;
+
+    beforeEach(async function() {
+      [owner,add1,add2,...addrs]=await ethers.getSigners();
+       token=await ethers.getContractFactory("token");
+       hardhattoken=await token.deploy();
+      
+    });
+
+
+     describe("deplloy",function(){
+       it("check for owner",async function(){
+           expect(await hardhattoken.owner()).to.equal(owner.address);
+       })
+     })
+
+
+
 })
 
